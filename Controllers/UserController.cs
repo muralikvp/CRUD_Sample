6626574916d401031ead59d5304f8f40c0731b96
Murali Krishnan <murali@hopetutors.com>
2023-05-19 10:30:42 +0530

    Second Commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using WebApp1.Models;
 
@@ -8,6 +9,20 @@ namespace WebApp1.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
+
+
+        //[Route("HopeTutors/api")]
+
+        //localhost:33/HopeTutors/api/GetUser
+
+        //[Route("[controller]/api")]
+
+        //localhost:33/user/api/GetUser
+
+        //[Route("Dev/[controller]/api")]
+
+        //localhost:33/Dev/user/api/GetUser
+
         private readonly ILogger<UserController> _logger;
         private readonly HTContext _context;
         public UserController(ILogger<UserController> logger, HTContext context)
@@ -16,7 +31,8 @@ namespace WebApp1.Controllers
             _context = context;
         }
 
-        [HttpGet(Name = "GetUsers")]
+        [HttpGet]
+        [Route("GetAllUsers")]
         public IEnumerable<User> GetUser()
         {
             //select * from user 
@@ -24,16 +40,67 @@ namespace WebApp1.Controllers
             return _context.user.ToList();
         }
 
-        [HttpPost(Name = "PostUser")]
+        [HttpPost]
+        [Route("InsertUser")]
         public ActionResult<User> InsertUser(User userOb)
         {
             //insert into[HTInfo_1].[dbo].[User] values('krishnan', 'krishnankvp', 'test', 1)
             _context.user.Add(userOb);
             _context.SaveChanges();
-
             return CreatedAtAction("GetUsers", new { id = userOb.Id }, userOb);
-
-
         }
+
+        [HttpGet]
+        [Route("GetUser")]
+        public ActionResult<User> getUser(int id)
+        {
+
+            //select * from user where id = 12;
+            var _user = _context.user.Find(id);
+            if (_user == null)
+            {
+                return NotFound();
+            }
+            return _user;
+        }
+
+        [HttpPost]
+        [Route("UpdateUser")]
+        public ActionResult<User> UpdateUser(User user)
+        {
+
+            //update user set name = Hari,userName = Dani id = 12;
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+            return user;
+
+            //var _user = _context.user.Find(user.Id);
+            //if (_user == null)
+            //{
+            //    return NotFound();
+            //}
+            //else
+            //{
+               
+            //}
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public ActionResult<User> deleteUser(int id)
+        { 
+            var _user = _context.user.Find(id);
+            if (_user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.user.Remove(_user);
+                _context.SaveChanges();
+                return Ok();
+            }
+        }
+
     }
-}
+    }
